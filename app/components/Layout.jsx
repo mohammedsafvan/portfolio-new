bd092f7ac83f650ec18d2e62a534e4fc385d7d30
Mohammed Safvan <mohammedsafvannk@gmail.com>
2023-09-24 00:32:37 +0530

    head title, favicon upodated, data added, Projects section started " ;

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -2,12 +2,14 @@
 import { ThemeProvider } from "next-themes";
 import NavBar from "./NavBar";
 import Hero from "./Hero";
+import Projects from "./Projects";
 
 const Layout = () => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <NavBar />
       <Hero />
+      <Projects/>
     </ThemeProvider>
   );
 };
--- app/utils/data.js
+++ app/utils/data.js
@@ -2,7 +2,11 @@ import {
   AiOutlineMail,
   AiOutlineLinkedin,
   AiOutlineGithub,
-  AiOutlineGoogle
+  AiOutlineGoogle,
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillGoogleCircle,
+  AiFillMail,
 } from "react-icons/ai";
 
 export const buttonData = [
@@ -41,5 +45,129 @@ export const buttonData = [
     text: "Github",
     icon: <AiOutlineGithub size={25} />,
     url: "https://github.com/mohammedsafvan/",
+  },
+];
+
+export const techStacks = [
+  {
+    name: "Flutter",
+    url: "https://flutter.dev/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/flutter-colored.svg",
+  },
+  {
+    name: "HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg",
+  },
+  {
+    name: "CSS",
+    url: "https://www.w3.org/TR/CSS/#css",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg",
+  },
+  {
+    name: "JavaScript",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
+  },
+  {
+    name: "React",
+    url: "https://reactjs.org/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg",
+  },
+  {
+    name: "NextJs",
+    url: "https://nextjs.org/docs",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nextjs-dark.svg",
+  },
+  {
+    name: "Tailwind CSS",
+    url: "https://tailwindcss.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg",
+  },
+  {
+    name: "NodeJS",
+    url: "https://nodejs.org/en/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg",
+  },
+  {
+    name: "Express",
+    url: "https://expressjs.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-dark.svg",
+  },
+  {
+    name: "MySQL",
+    url: "https://www.mysql.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg",
+  },
+  {
+    name: "MongoDB",
+    url: "https://www.mongodb.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg",
+  },
+  {
+    name: "FireBase",
+    url: "https://firebase.google.com/",
+    imageUrl:
+      "https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/firebase-colored.svg",
+  },
+];
+
+export const projects = [
+  {
+    name: "Todo Firebase",
+    techStack: ["Flutter", "Firebase"],
+    image: "/todo_firebase.png",
+    description:
+      "A todo Android app with firebase cloud firestore and authentication(Phone No, Google, Email) with persistant storage",
+    url: "https://github.com/mohammedsafvan/todo_firebase",
+  },
+  {
+    name: "Note Taking app",
+    techStack: ["React", "Markdown"],
+    image:
+      "https://raw.githubusercontent.com/mohammedsafvan/note-taking-app/main/note-app.png",
+    description: "A minimalistic note taking app based on markdown. ",
+    url: "https://github.com/mohammedsafvan/note-taking-app",
+  },
+  {
+    name: "Portfolio",
+    techStack: ["NextJs", "Tailwind CSS"],
+    image: "/portfolio.png",
+    description: "This website, which is made with NextJs and Tailwind CSS",
+    url: "https://github.com/mohammedsafvan/portfolio-safvan",
+  },
+];
+
+export const socials = [
+  {
+    name: "LinkedIn",
+    Icon: <AiFillLinkedin className="hover:scale-125 duration-500" />,
+    url: "https://www.linkedin.com/in/mohammed-safvan-7022b21a3/",
+  },
+  {
+    name: "Github",
+    Icon: <AiFillGithub className=" hover:scale-125 duration-500" />,
+    url: "https://github.com/mohammedsafvan",
+  },
+  {
+    name: "GoogleDev",
+    Icon: <AiFillGoogleCircle className="   duration-500 hover:scale-125" />,
+    url: "https://g.dev/mohammedsafvan",
+  },
+  {
+    name: "Mail",
+    Icon: <AiFillMail className="duration-500 hover:scale-125" />,
+    url: "mailto:mohammedsafvan.me@gmail.com",
   },
 ];
