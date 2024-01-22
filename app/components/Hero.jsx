e69b9adf8cc33970add2de0eff5ab6a3db4738a6
Mohammed Safvan <mohammedsafvannk@gmail.com>
2024-01-22 21:24:18 +0530

    padding edits, removed details

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,22 +4,22 @@ import { buttonData } from "../utils/data";
 const Hero = () => {
   return (
     <>
-      <div id="1" className="pt-16 bg-white dark:bg-black h-screen px-10 ">
-        <h3 className="pt-12 pb-5 font-montserrat text-2xl text-gray-500  dark:text-gray-400 ">
+      <div id="1" className="p-12  md:py-24 bg-white dark:bg-black h-screen  ">
+        <h3 className="pb-10  font-montserrat text-2xl text-gray-500  dark:text-gray-400 ">
           Hey, I am
         </h3>
-        <h1 className="pb-4 md:pt-14 leading-[100%] font-montserrat  text-transparent bg-clip-text bg-gradient-to-r from-[#9845E8]  via-[#33D2FF] to-[#DD5789] font-bold text-[3.5rem]">
+        <h1 className=" pb-10 md:pb-20 leading-[100%] font-montserrat  text-transparent bg-clip-text bg-gradient-to-r from-[#9845E8]  via-[#33D2FF] to-[#DD5789] font-bold text-[3.5rem]">
           Mohammed Safvan
         </h1>
 
-        <p className="font-montserrat md:tracking-wider pt-16 md:text-2xl  text-lg text-justify text-black dark:text-white">
+        <p className="pb-10 md:pb-20 font-montserrat md:tracking-wider md:text-2xl  text-lg text-justify text-black dark:text-white">
           An individual with passion for coding. Currently pursuing
           undergraduates from Sree Chitra Thirunal College of Engineering. I
           Likes to work with new technologies. Interested in Artificial
           Intelligence and Machine Learning. Familiar with popular technologies
           of web, mobile AI & ML
         </p>
-        <div className="mt-7 gap-6 grid grid-cols-2 md:grid-cols-4 md:pt-24">
+        <div className="pb-10 md:pb-20  gap-6 grid grid-cols-2 md:grid-cols-4 ">
           {buttonData.map((element) => (
             <Button
               key={element.id}
@@ -33,7 +33,6 @@ const Hero = () => {
           ))}
         </div>
       </div>
-      <div className="min-h-[1px] hidden justify-center lg:block bg-gray-300 "></div>
     </>
   );
 };
--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -1,15 +1,11 @@
 "use client";
 import { ThemeProvider } from "next-themes";
-import NavBar from "./NavBar";
 import Hero from "./Hero";
-import Projects from "./Projects";
 
 const Layout = () => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
-      <NavBar />
       <Hero />
-      <Projects />
     </ThemeProvider>
   );
 };
